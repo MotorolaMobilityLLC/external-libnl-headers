thom311/libnl

commit 81d2b1d509d91fe894e4aec0a6a76b0bf75514cd

Add support for inet diag Netlink protocol.
- Inet diag allows users to gather low-level socket information.
- This library provides a higher-level API for creating inetdiag requests (via
  idiagnl_connect and idiagnl_send_simple) and parsing the replies (via
  idiagnl_msg_parse). A cache is also provided (via idiagnl_msg_alloc_cache).
- Request and message objects provide APIs for accessing and setting the
  various properties of each.
- This library also allows the user to parse the inetdiag response attributes
  which contain information about traffic class, TOS, congestion, socket
  memory info, and more depending on the kernel version used.
- Includes doxygen documentation.

include/netlink/idiag/msg.h
@@ -1,0 +1,83 @@
+/*
+ * netlink/idiag/msg.h		Inetdiag Netlink Message
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
+ */
+
+#ifndef NETLINK_IDIAGNL_MSG_H_
+#define NETLINK_IDIAGNL_MSG_H_
+
+#include <netlink/netlink.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+struct idiagnl_msg;
+extern struct nl_object_ops  idiagnl_msg_obj_ops;
+
+extern struct idiagnl_msg * idiagnl_msg_alloc(void);
+extern int		idiagnl_msg_alloc_cache(struct nl_sock *, int, int,
+                                                struct nl_cache**);
+extern void		idiagnl_msg_get(struct idiagnl_msg *);
+extern void		idiagnl_msg_put(struct idiagnl_msg *);
+extern uint8_t		idiagnl_msg_get_family(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_family(struct idiagnl_msg *, uint8_t);
+extern uint8_t		idiagnl_msg_get_state(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_state(struct idiagnl_msg *, uint8_t);
+extern uint8_t		idiagnl_msg_get_timer(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_timer(struct idiagnl_msg *, uint8_t);
+extern uint8_t		idiagnl_msg_get_retrans(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_retrans(struct idiagnl_msg *, uint8_t);
+extern uint16_t		idiagnl_msg_get_sport(struct idiagnl_msg *);
+extern void		idiagnl_msg_set_sport(struct idiagnl_msg *, uint16_t);
+extern uint16_t		idiagnl_msg_get_dport(struct idiagnl_msg *);
+extern void		idiagnl_msg_set_dport(struct idiagnl_msg *, uint16_t);
+extern struct nl_addr *	idiagnl_msg_get_src(const struct idiagnl_msg *);
+extern int		idiagnl_msg_set_src(struct idiagnl_msg *,
+                                            struct nl_addr *);
+extern struct nl_addr *	idiagnl_msg_get_dst(const struct idiagnl_msg *);
+extern int		idiagnl_msg_set_dst(struct idiagnl_msg *,
+		                            struct nl_addr *);
+extern uint32_t		idiagnl_msg_get_ifindex(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_ifindex(struct idiagnl_msg *, uint32_t);
+extern uint32_t		idiagnl_msg_get_expires(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_expires(struct idiagnl_msg *, uint32_t);
+extern uint32_t		idiagnl_msg_get_rqueue(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_rqueue(struct idiagnl_msg *, uint32_t);
+extern uint32_t		idiagnl_msg_get_wqueue(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_wqueue(struct idiagnl_msg *, uint32_t);
+extern uint32_t		idiagnl_msg_get_uid(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_uid(struct idiagnl_msg *, uint32_t);
+extern uint32_t		idiagnl_msg_get_inode(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_inode(struct idiagnl_msg *, uint32_t);
+extern uint8_t		idiagnl_msg_get_tos(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_tos(struct idiagnl_msg *, uint8_t);
+extern uint8_t		idiagnl_msg_get_tclass(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_tclass(struct idiagnl_msg *, uint8_t);
+extern uint8_t		idiagnl_msg_get_shutdown(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_shutdown(struct idiagnl_msg *, uint8_t);
+extern char *		idiagnl_msg_get_cong(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_cong(struct idiagnl_msg *, char *);
+extern struct idiagnl_meminfo *idiagnl_msg_get_meminfo(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_meminfo(struct idiagnl_msg *,
+                                                struct idiagnl_meminfo *);
+extern struct idiagnl_vegasinfo *idiagnl_msg_get_vegasinfo(const struct idiagnl_msg *);
+extern void		idiagnl_msg_set_vegasinfo(struct idiagnl_msg *,
+                                                  struct idiagnl_vegasinfo *);
+extern struct tcp_info idiagnl_msg_get_tcpinfo(const struct idiagnl_msg *);
+extern void	       idiagnl_msg_set_tcpinfo(struct idiagnl_msg *,
+                                               struct tcp_info *);
+
+extern int		idiagnl_msg_parse(struct nlmsghdr *,
+                                          struct idiagnl_msg **);
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* NETLINK_IDIAGNL_MSG_H_ */

doc/Doxyfile.in
@@ -197,8 +197,10 @@ ALIASES                = arg=\param \
                          "ref_asciidoc{3}=<a href=\"../\1.html#\2\"><b>\3</b></a>" \
                          "ref_core{2}=\ref_asciidoc{core,\1,\2 (Netlink Core Library Development Guide)}" \
                          "ref_route{2}=\ref_asciidoc{route,\1,\2 (Netlink Routing Development Guide)}" \
+                         "ref_idiagnl{2}=\ref_asciidoc{idiag,\1,\2 (Netlink Inet Diag Development Guide)}" \
                          "core_doc{2}=\ref_core{\1,\2}" \
                          "route_doc{2}=\ref_route{\1,\2}" \
+                         "idiagnl_doc{2}=\ref_idiagnl{\1,\2}" \
 			 "callback=\par Callback Invocation:\n" \
 			 "lowlevel=\copydoc low_level_api"
 

include/Makefile.am
@@ -78,8 +78,12 @@ nobase_libnlinclude_HEADERS = \
 	netlink/object-api.h \
 	netlink/route/link/api.h \
 	netlink/route/link/info-api.h \
-	netlink/route/tc-api.h
-
+	netlink/route/tc-api.h \
+	netlink/idiag/idiagnl.h \
+	netlink/idiag/meminfo.h \
+	netlink/idiag/msg.h \
+	netlink/idiag/req.h \
+	netlink/idiag/vegasinfo.h
 
 if ENABLE_CLI
 nobase_libnlinclude_HEADERS += \

include/netlink-private/types.h
@@ -7,6 +7,7 @@
  *	of the License.
  *
  * Copyright (c) 2003-2013 Thomas Graf <tgraf@suug.ch>
+ * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
  */
 
 #ifndef NETLINK_LOCAL_TYPES_H_
@@ -17,6 +18,7 @@
 #include <netlink/route/qdisc.h>
 #include <netlink/route/rtnl.h>
 #include <netlink/route/route.h>
+#include <netlink/idiag/idiagnl.h>
 #include <netlink-private/route/tc-api.h>
 
 #define NL_SOCK_BUFSIZE_SET	(1<<0)
@@ -909,4 +911,61 @@ struct ematch_quoted {
 	int	index;
 };
 
+struct idiagnl_meminfo {
+	NLHDR_COMMON
+
+	uint32_t idiag_rmem;
+	uint32_t idiag_wmem;
+	uint32_t idiag_fmem;
+	uint32_t idiag_tmem;
+};
+
+struct idiagnl_vegasinfo {
+	NLHDR_COMMON
+
+	uint32_t tcpv_enabled;
+	uint32_t tcpv_rttcnt;
+	uint32_t tcpv_rtt;
+	uint32_t tcpv_minrtt;
+};
+
+struct idiagnl_msg {
+	NLHDR_COMMON
+
+	uint8_t			    idiag_family;
+	uint8_t			    idiag_state;
+	uint8_t			    idiag_timer;
+	uint8_t			    idiag_retrans;
+	uint16_t		    idiag_sport;
+	uint16_t		    idiag_dport;
+	struct nl_addr *	    idiag_src;
+	struct nl_addr *	    idiag_dst;
+	uint32_t		    idiag_ifindex;
+	uint32_t		    idiag_expires;
+	uint32_t		    idiag_rqueue;
+	uint32_t		    idiag_wqueue;
+	uint32_t		    idiag_uid;
+	uint32_t		    idiag_inode;
+
+	uint8_t			    idiag_tos;
+	uint8_t			    idiag_tclass;
+	uint8_t			    idiag_shutdown;
+	char *			    idiag_cong;
+	struct idiagnl_meminfo *    idiag_meminfo;
+	struct idiagnl_vegasinfo *  idiag_vegasinfo;
+	struct tcp_info		    idiag_tcpinfo;
+	uint32_t		    idiag_skmeminfo[IDIAG_SK_MEMINFO_VARS];
+};
+
+struct idiagnl_req {
+	NLHDR_COMMON
+
+	uint8_t			idiag_family;
+	uint8_t			idiag_ext;
+	struct nl_addr *	idiag_src;
+	struct nl_addr *	idiag_dst;
+	uint32_t		idiag_ifindex;
+	uint32_t		idiag_states;
+	uint32_t		idiag_dbs;
+};
 #endif

include/netlink/idiag/idiagnl.h
@@ -1,0 +1,126 @@
+/*
+ * netlink/idiag/idiagnl.h		Inetdiag Netlink
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
+ */
+
+#ifndef NETLINK_IDIAGNL_H_
+#define NETLINK_IDIAGNL_H_
+
+#include <netlink/netlink.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Inet Diag message types
+ */
+#define IDIAG_TCPDIAG_GETSOCK	18
+#define IDIAG_DCCPDIAG_GETSOCK	19
+#define IDIAG_GETSOCK_MAX	24
+
+/**
+ * Socket state identifiers
+ * @ingroup idiag
+ */
+enum {
+	IDIAG_SS_UNKNOWN,
+	IDIAG_SS_ESTABLISHED,
+	IDIAG_SS_SYN_SENT,
+	IDIAG_SS_SYN_RECV,
+	IDIAG_SS_FIN_WAIT1,
+	IDIAG_SS_FIN_WAIT2,
+	IDIAG_SS_TIME_WAIT,
+	IDIAG_SS_CLOSE,
+	IDIAG_SS_CLOSE_WAIT,
+	IDIAG_SS_LAST_ACK,
+	IDIAG_SS_LISTEN,
+	IDIAG_SS_CLOSING,
+	IDIAG_SS_MAX
+};
+
+/**
+ * Macro to represent all socket states.
+ * @ingroup idiag
+ */
+#define IDIAG_SS_ALL ((1<<IDIAG_SS_MAX)-1)
+
+/**
+ * Inet Diag extended attributes
+ * @ingroup idiag
+ */
+enum {
+	IDIAG_ATTR_NONE,
+	IDIAG_ATTR_MEMINFO,
+	IDIAG_ATTR_INFO,
+	IDIAG_ATTR_VEGASINFO,
+	IDIAG_ATTR_CONG,
+	IDIAG_ATTR_TOS,
+	IDIAG_ATTR_TCLASS,
+	IDIAG_ATTR_SKMEMINFO,
+	IDIAG_ATTR_SHUTDOWN,
+	IDIAG_ATTR_MAX,
+};
+
+/**
+ * Macro to represent all socket attributes.
+ * @ingroup idiag
+ */
+#define IDIAG_ATTR_ALL ((1<<IDIAG_ATTR_MAX)-1)
+
+/**
+ * Socket memory info identifiers
+ * @ingroup idiag
+ */
+enum {
+	IDIAG_SK_MEMINFO_RMEM_ALLOC,
+	IDIAG_SK_MEMINFO_RCVBUF,
+	IDIAG_SK_MEMINFO_WMEM_ALLOC,
+	IDIAG_SK_MEMINFO_SNDBUF,
+	IDIAG_SK_MEMINFO_FWD_ALLOC,
+	IDIAG_SK_MEMINFO_WMEM_QUEUED,
+	IDIAG_SK_MEMINFO_OPTMEM,
+	IDIAG_SK_MEMINFO_BACKLOG,
+
+	IDIAG_SK_MEMINFO_VARS,
+};
+
+/**
+ * Socket timer indentifiers
+ * @ingroupd idiag
+ */
+enum {
+	IDIAG_TIMER_OFF,
+	IDIAG_TIMER_ON,
+	IDIAG_TIMER_KEEPALIVE,
+	IDIAG_TIMER_TIMEWAIT,
+	IDIAG_TIMER_PERSIST,
+	IDIAG_TIMER_UNKNOWN,
+};
+
+extern char *	idiagnl_state2str(int, char *, size_t);
+extern int	idiagnl_str2state(const char *);
+
+extern int	idiagnl_connect(struct nl_sock *);
+extern int	idiagnl_send_simple(struct nl_sock *, int, uint8_t, uint16_t,
+                                    uint16_t);
+
+extern char *		idiagnl_timer2str(int, char *, size_t);
+extern int		idiagnl_str2timer(const char *);
+extern char *		idiagnl_attrs2str(int, char *, size_t);
+extern char *		idiagnl_tcpstate2str(uint8_t, char *, size_t);
+extern char *		idiagnl_tcpopts2str(uint8_t, char *, size_t);
+extern char *		idiagnl_shutdown2str(uint8_t, char *, size_t);
+extern char *		idiagnl_exts2str(uint8_t, char *, size_t);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* NETLINK_IDIAGNL_H_ */

include/netlink/idiag/meminfo.h
@@ -1,0 +1,41 @@
+/*
+ * netlink/idiag/meminfo.h		Inetdiag Netlink Memory Info
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
+ */
+
+#ifndef NETLINK_IDIAGNL_MEMINFO_H_
+#define NETLINK_IDIAGNL_MEMINFO_H_
+
+#include <netlink/netlink.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+extern struct nl_object_ops	idiagnl_meminfo_obj_ops;
+
+extern struct idiagnl_meminfo *idiagnl_meminfo_alloc(void);
+extern void idiagnl_meminfo_get(struct idiagnl_meminfo *);
+extern void idiagnl_meminfo_put(struct idiagnl_meminfo *);
+
+extern uint32_t	  idiagnl_meminfo_get_rmem(const struct idiagnl_meminfo *);
+extern uint32_t   idiagnl_meminfo_get_wmem(const struct idiagnl_meminfo *);
+extern uint32_t   idiagnl_meminfo_get_fmem(const struct idiagnl_meminfo *);
+extern uint32_t   idiagnl_meminfo_get_tmem(const struct idiagnl_meminfo *);
+
+extern void	  idiagnl_meminfo_set_rmem(struct idiagnl_meminfo *, uint32_t);
+extern void	  idiagnl_meminfo_set_wmem(struct idiagnl_meminfo *, uint32_t);
+extern void	  idiagnl_meminfo_set_fmem(struct idiagnl_meminfo *, uint32_t);
+extern void	  idiagnl_meminfo_set_tmem(struct idiagnl_meminfo *, uint32_t);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* NETLINK_IDIAGNL_MEMINFO_H_ */

include/netlink/idiag/req.h
@@ -1,0 +1,50 @@
+/*
+ * netlink/idiag/req.h		Inetdiag Netlink Request
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
+ */
+
+#ifndef NETLINK_IDIAGNL_REQ_H_
+#define NETLINK_IDIAGNL_REQ_H_
+
+#include <netlink/netlink.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+struct idiagnl_req;
+extern struct nl_object_ops	idiagnl_req_obj_ops;
+
+extern struct idiagnl_req * idiagnl_req_alloc(void);
+extern void		    idiagnl_req_get(struct idiagnl_req *);
+extern void		    idiagnl_req_put(struct idiagnl_req *);
+extern uint8_t		    idiagnl_req_get_family(const struct idiagnl_req *);
+extern void		    idiagnl_req_set_family(struct idiagnl_req *,
+                                                   uint8_t);
+extern uint8_t		    idiagnl_req_get_ext(const struct idiagnl_req *);
+extern void		    idiagnl_req_set_ext(struct idiagnl_req *, uint8_t);
+extern uint32_t		    idiagnl_req_get_ifindex(const struct idiagnl_req *);
+extern void		    idiagnl_req_set_ifindex(struct idiagnl_req *,
+                                                    uint32_t);
+extern uint32_t		    idiagnl_req_get_states(const struct idiagnl_req *);
+extern void		    idiagnl_req_set_states(struct idiagnl_req *,
+                                                   uint32_t);
+extern uint32_t		    idiagnl_req_get_dbs(const struct idiagnl_req *);
+extern void		    idiagnl_req_set_dbs(struct idiagnl_req *, uint32_t);
+extern struct nl_addr *	    idiagnl_req_get_src(const struct idiagnl_req *);
+extern int		    idiagnl_req_set_src(struct idiagnl_req *,
+                                                struct nl_addr *);
+extern struct nl_addr *	    idiagnl_req_get_dst(const struct idiagnl_req *);
+extern int		    idiagnl_req_set_dst(struct idiagnl_req *,
+                                                struct nl_addr *);
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* NETLINK_IDIAGNL_REQ_H_ */

include/netlink/idiag/vegasinfo.h
@@ -1,0 +1,43 @@
+/*
+ * netlink/idiag/vegasinfo.h		Inetdiag Netlink TCP Vegas Info
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
+ */
+
+#ifndef NETLINK_IDIAGNL_VEGASINFO_H_
+#define NETLINK_IDIAGNL_VEGASINFO_H_
+
+#include <netlink/netlink.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+extern struct nl_object_ops	  idiagnl_vegasinfo_obj_ops;
+extern struct idiagnl_vegasinfo * idiagnl_vegasinfo_alloc(void);
+extern void	  idiagnl_vegasinfo_get(struct idiagnl_vegasinfo *);
+extern void	  idiagnl_vegasinfo_put(struct idiagnl_vegasinfo *);
+
+extern uint32_t idiagnl_vegasinfo_get_enabled(const struct idiagnl_vegasinfo *);
+extern uint32_t	idiagnl_vegasinfo_get_rttcnt(const struct idiagnl_vegasinfo *);
+extern uint32_t idiagnl_vegasinfo_get_rtt(const struct idiagnl_vegasinfo *);
+extern uint32_t idiagnl_vegasinfo_get_minrtt(const struct idiagnl_vegasinfo *);
+
+extern void	idiagnl_vegasinfo_set_enabled(struct idiagnl_vegasinfo *,
+                                              uint32_t);
+extern void	idiagnl_vegasinfo_set_rttcnt(struct idiagnl_vegasinfo *,
+                                             uint32_t);
+extern void	idiagnl_vegasinfo_set_rtt(struct idiagnl_vegasinfo *, uint32_t);
+extern void	idiagnl_vegasinfo_set_minrtt(struct idiagnl_vegasinfo *,
+                                             uint32_t);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* NETLINK_IDIAGNL_VEGASINFO_H_ */

MotorolaMobilityLLC/external-libnl-headers

commit a81e4f8d18dea437b0250f4b77ce9d99455aafc6

idiag: deprecate idiagnl_msg_obj_ops variable in public API
This internal structure should never have been exposed publically.
It as introduced as public API in v3.2.23.

For now, don't remove it as it would be an API/ABI break.

Fixes: 81d2b1d509d91fe894e4aec0a6a76b0bf75514cd
Acked-by: Thomas Graf <tgraf@suug.ch>
Signed-off-by: Thomas Haller <thaller@redhat.com>

include/netlink/idiag/msg.h
@@ -19,6 +19,8 @@ extern "C" {
 #endif /* __cplusplus */
 
 struct idiagnl_msg;
+
+/* @deprecated: DO NOT USE this variable. */
 extern struct nl_object_ops  idiagnl_msg_obj_ops;
 
 extern struct idiagnl_msg * idiagnl_msg_alloc(void);