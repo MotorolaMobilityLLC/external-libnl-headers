thom311/libnl

commit 9c066b9271493ce3efff0b9d7b6e424990bba3f2

idiag: provide a hash function for idiag objects
Without ->oo_keygen, libnl will use linear search
for cache objects. This is extremely slow for idiag
when we have a lot of TCP connections. Provide a
hash function for idiag so that libnl will be able
to lookup a hashtable.

http://lists.infradead.org/pipermail/libnl/2014-November/001715.html

Cc: Thomas Graf <tgraf@suug.ch>
Cc: Thomas Haller <thaller@redhat.com>
Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
Signed-off-by: Thomas Haller <thaller@redhat.com>

lib/idiag/idiag_msg_obj.c
@@ -10,6 +10,7 @@
  */
 
 #include <netlink-private/netlink.h>
+#include <netlink/hashtable.h>
 #include <netlink/idiag/msg.h>
 #include <netlink/idiag/meminfo.h>
 #include <netlink/idiag/vegasinfo.h>
@@ -710,6 +711,32 @@ errout_nomem:
 	goto errout;
 }
 
+static void idiagnl_keygen(struct nl_object *obj, uint32_t *hashkey,
+        uint32_t table_sz)
+{
+	struct idiagnl_msg *msg = (struct idiagnl_msg *)obj;
+	unsigned int key_sz;
+	struct idiagnl_hash_key {
+		uint8_t	family;
+		uint8_t	state;
+		uint16_t sport;
+		uint16_t dport;
+	} __attribute__((packed)) key;
+
+	key_sz = sizeof(key);
+	key.family = msg->idiag_family;
+	key.state = msg->idiag_state;
+	key.sport = msg->idiag_sport;
+	key.dport = msg->idiag_dport;
+
+	*hashkey = nl_hash(&key, key_sz, 0) % table_sz;
+
+	NL_DBG(5, "idiagnl %p key (fam %d state %d sport %d dport %d) keysz %d, hash 0x%x\n",
+	       msg, key.family, key.state, key.sport, key.dport, key_sz, *hashkey);
+
+	return;
+}
+
 /** @cond SKIP */
 struct nl_object_ops idiagnl_msg_obj_ops = {
 	.oo_name			 = "idiag/idiag_msg",
@@ -721,6 +748,7 @@ struct nl_object_ops idiagnl_msg_obj_ops = {
 		[NL_DUMP_DETAILS]	 = idiag_msg_dump_details,
 		[NL_DUMP_STATS]		 = idiag_msg_dump_stats,
 	},
+	.oo_keygen			= idiagnl_keygen,
 	.oo_attrs2str			= idiagnl_attrs2str,
 	.oo_id_attrs			= (IDIAG_ATTR_INFO)
 };

MotorolaMobilityLLC/external-libnl-headers

commit ba6a95291529e1a7b0486fadc65ac2362655e883

idiag: add a oo_compare() function for idiag_msg objects
Having a oo_keygen() function only makes sense together with a
oo_compare() function because after hashing, you still have to compare
the objects for equality (in case of hash collission).

Fixes: 9c066b9271493ce3efff0b9d7b6e424990bba3f2
Acked-by: Thomas Graf <tgraf@suug.ch>
Signed-off-by: Thomas Haller <thaller@redhat.com>

lib/idiag/idiag_msg_obj.c
@@ -851,6 +851,22 @@ static char *_idiagnl_attrs2str(int attrs, char *buf, size_t len)
 	                   ARRAY_SIZE(idiagnl_attrs));
 }
 
+static int idiagnl_compare(struct nl_object *_a, struct nl_object *_b,
+                           uint32_t attrs, int flags)
+{
+	struct idiagnl_msg *a = (struct idiagnl_msg *) _a;
+	struct idiagnl_msg *b = (struct idiagnl_msg *) _b;
+	int diff = 0;
+
+#define _DIFF(ATTR, EXPR) ATTR_DIFF(attrs, IDIAGNL_ATTR_##ATTR, a, b, EXPR)
+	diff |= _DIFF(FAMILY, a->idiag_family != b->idiag_family);
+	diff |= _DIFF(STATE,  a->idiag_state != b->idiag_state);
+	diff |= _DIFF(SPORT,  a->idiag_sport != b->idiag_sport);
+	diff |= _DIFF(DPORT,  a->idiag_dport != b->idiag_dport);
+#undef _DIFF
+	return diff;
+}
+
 static void idiagnl_keygen(struct nl_object *obj, uint32_t *hashkey,
         uint32_t table_sz)
 {
@@ -888,6 +904,7 @@ struct nl_object_ops idiagnl_msg_obj_ops = {
 		[NL_DUMP_DETAILS]	 = idiag_msg_dump_details,
 		[NL_DUMP_STATS]		 = idiag_msg_dump_stats,
 	},
+	.oo_compare			= idiagnl_compare,
 	.oo_keygen			= idiagnl_keygen,
 	.oo_attrs2str			= _idiagnl_attrs2str,
 	.oo_id_attrs                    = (IDIAGNL_ATTR_FAMILY |