thom311/libnl

commit bbdcaea9a779885fedc04817dcc11953a377bfd5

lib: return error if an incomplete message was read
If recvmsg indicates that the message read was truncated libnl retries
to read the complete message after increasing the message buffer. This
only works if the message flags MSG_PEEK | MSG_TRUNC are set. If
NL_MSG_PEEK is not enabled on the nl_sock structure, flags are left
empty and the rest of the truncated message is discarded, hence a
subsequent recvmsg returns the next message (in case of a multipart
message, the NLMSG_DONE) is read and returned.
This patch aborts message processing if the message was truncated and
the NL_MSG_PEEK flags was not activated for the nl_sock structure.

http://lists.infradead.org/pipermail/libnl/2015-June/001888.html

[thaller@redhat.com: add NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK]

Signed-off-by: Thomas Egerer <hakke_007@gmx.de>
Signed-off-by: Thomas Haller <thaller@redhat.com>

include/netlink/utils.h
@@ -135,6 +135,14 @@ enum {
 	NL_CAPABILITY_VERSION_3_2_26 = 7,
 #define NL_CAPABILITY_VERSION_3_2_26 NL_CAPABILITY_VERSION_3_2_26
 
+	/**
+	 * nl_recv() fails with NLE_MSG_TRUNC if a message got truncated
+	 * with NL_MSG_PEEK disabled. Previously, the failed message was wrongly
+	 * discarded and the next message received.
+	 */
+	NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK = 8,
+#define NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK
+
 	__NL_CAPABILITY_MAX,
 	NL_CAPABILITY_MAX = (__NL_CAPABILITY_MAX - 1),
 #define NL_CAPABILITY_MAX NL_CAPABILITY_MAX

lib/utils.c
@@ -1150,7 +1150,7 @@ int nl_has_capability (int capability)
 			NL_CAPABILITY_ROUTE_LINK_GET_KERNEL_FAIL_OPNOTSUPP,
 			NL_CAPABILITY_ROUTE_ADDR_COMPARE_CACHEINFO,
 			NL_CAPABILITY_VERSION_3_2_26,
-			0),
+			NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK),
 		/* IMPORTANT: these capability numbers are intended to be universal and stable
 		 * for libnl3. Don't allocate new numbers on your own that differ from upstream
 		 * libnl3.

lib/nl.c
@@ -725,6 +725,13 @@ retry:
 
 	if (iov.iov_len < n || (msg.msg_flags & MSG_TRUNC)) {
 		void *tmp;
+
+		/* respond with error to an incomplete message */
+		if (!(sk->s_flags & NL_MSG_PEEK)) {
+			retval = -NLE_MSG_TRUNC;
+			goto abort;
+		}
+
 		/* Provided buffer is not long enough, enlarge it
 		 * to size of n (which should be total length of the message)
 		 * and try again. */

MotorolaMobilityLLC/external-libnl-headers

commit 285b44ba6ea1c28f23916f06a659949cd8ba200a

lib: fix spelling of NL_CAPABILITY_NL_RECV_FAIL_TRUNC_NO_PEEK
Instead of NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK.

Fixes: bbdcaea9a779885fedc04817dcc11953a377bfd5

Signed-off-by: Thomas Haller <thaller@redhat.com>

include/netlink/utils.h
@@ -140,8 +140,8 @@ enum {
 	 * with NL_MSG_PEEK disabled. Previously, the failed message was wrongly
 	 * discarded and the next message received.
 	 */
-	NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK = 8,
-#define NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK
+	NL_CAPABILITY_NL_RECV_FAIL_TRUNC_NO_PEEK = 8,
+#define NL_CAPABILITY_NL_RECV_FAIL_TRUNC_NO_PEEK NL_CAPABILITY_NL_RECV_FAIL_TRUNC_NO_PEEK
 
 	__NL_CAPABILITY_MAX,
 	NL_CAPABILITY_MAX = (__NL_CAPABILITY_MAX - 1),

lib/utils.c
@@ -1150,7 +1150,7 @@ int nl_has_capability (int capability)
 			NL_CAPABILITY_ROUTE_LINK_GET_KERNEL_FAIL_OPNOTSUPP,
 			NL_CAPABILITY_ROUTE_ADDR_COMPARE_CACHEINFO,
 			NL_CAPABILITY_VERSION_3_2_26,
-			NL_CAPABILITY_NL_RECV_FAIL_TRUNK_NO_PEEK),
+			NL_CAPABILITY_NL_RECV_FAIL_TRUNC_NO_PEEK),
 		/* IMPORTANT: these capability numbers are intended to be universal and stable
 		 * for libnl3. Don't allocate new numbers on your own that differ from upstream
 		 * libnl3.